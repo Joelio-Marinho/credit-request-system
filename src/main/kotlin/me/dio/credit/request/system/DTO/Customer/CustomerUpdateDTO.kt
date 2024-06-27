cf3e5940374a8cc1681966c5ce206c908be092f9
Joelio Marinho <joeliomarinho@gmail.com>
2024-06-27 11:25:46 -0300

    Adição das validations para tratamento de erros

--- src/main/kotlin/me/dio/credit/request/system/DTO/Customer/CustomerUpdateDTO.kt
+++ src/main/kotlin/me/dio/credit/request/system/DTO/Customer/CustomerUpdateDTO.kt
@@ -1,17 +1,28 @@
 package me.dio.credit.request.system.DTO.Customer
 
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import me.dio.credit.request.system.entity.Customer
 import java.math.BigDecimal
 
 data class CustomerUpdateDTO(
 
-        val firstName: String,
-        val lastName: String,
-        var income: BigDecimal,
-        val zipCode: String,
-        val street: String
-){
-    fun toEntty(customer: Customer): Customer{
+    @field:NotEmpty(message = "Invalid firstName")
+    val firstName: String,
+
+    @field:NotEmpty(message = "Invalid lastName")
+    val lastName: String,
+
+    @field:NotNull(message = "Invalid income")
+    var income: BigDecimal,
+
+    @field:NotEmpty(message = "Invalid zipCode")
+    val zipCode: String,
+
+    @field:NotEmpty(message = "Invalid street")
+    val street: String
+) {
+    fun toEntty(customer: Customer): Customer {
 
         customer.firstName = this.firstName
         customer.lastName = this.lastName
